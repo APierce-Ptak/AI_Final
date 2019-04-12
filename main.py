083d79927622fc1b52a699350798771fe5a32f94
kidc12 <kidc12@gmail.com>
2019-04-12 15:23:02 -0400

    working

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np # linear algebra
 import os # accessing directory structure
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
-debug = False
+debug = True
 
 # Print Dataset files
 if debug: print(os.listdir('./Dataset'))
@@ -22,69 +22,68 @@ if debug: print(dataset)
 # Print the shape of the dataset
 print(dataset.shape)
 
-# X AND Y CODE GOES HERE0
-i = 0
-outputs = []
-inputs = datasetapierce
-#Fills output array with last column of inputs 
-for row in dataset:
+# X AND Y CODE GOES HEREO
+# i = 0
+# outputs = []
+# inputs = dataset
 
-  print("BeforeRow ",i," \n",row)
-  sizeOfRow = len(row)-1
-  outputs.append(row[sizeOfRow])
+    # Fills output array with last column of inputs 
+# for row in dataset:
 
-  print("\n\n\n\n\n")
+#   if debug: print("BeforeRow ",i," \n",row)
+#   sizeOfRow = len(row)-1
+#   outputs.append(row[sizeOfRow])
+
+#   if debug: print("\n\n\n\n\n")
   
-  i+=1
-#input has its last column removed
-inputs = np.delete(dataset, 7, 1)
+#   i+=1
+#     # input has its last column removed
+# inputs = np.delete(dataset, 7, 1)
 
-print("Inputs:\n", inputs)
-print("\n\n\n")
-print("LastColumn:\n", outputs)
-
+# if debug: print("Inputs:\n", inputs)
+# if debug: print("\n\n\n")
+# if debug: print("LastColumn:\n", outputs)
 
 #################################
+
+X = dataset[:,0:7]
+Y = dataset[:, 7]
+print(X)
+print(Y)
+X, Y = np.arange(10).reshape((5, 2)), range(5)
+
 neigh = KNeighborsRegressor(n_neighbors=2)
-neigh.fit(X, y) 
-print(neigh.predict([[1.5]]))
+neigh.fit(X, Y) 
+# print(neigh.predict([[0.8]]))
 
 
 
-# BEHOLD THE OLD CODE
-# df1 = pd.read_csv('./Dataset/Admission_Predict.csv', delimiter=',')
-# df1.dataframeName = 'Admission_Predict.csv'
+## Initialisation
 
-# More data so we'll use this one
-# df1 = pd.read_csv('./Dataset/Admission_Predict_Ver1.1.csv', delimiter=',')
-# df1.dataframeName = 'Admission_Predict_Ver1.1.csv'
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# %matplotlib inline
 
-# Drop Serial No. Column as datafrom include index
-# df1.drop(df1.columns[0],axis=1,inplace=True)
-# print(df1.head(5))
+# df = pd.DataFrame({
+#     'x': [12, 20, 28, 18, 29, 33, 24, 45, 45, 52, 51, 52, 55, 53, 55, 61, 64, 69, 72],
+#     'y': [39, 36, 30, 52, 54, 46, 55, 59, 63, 70, 66, 63, 58, 23, 14, 8, 19, 7, 24]
+# })
 
-# nRow, nCol = df1.shape
-# print("There are "+ str(nRow) + " rows and " + str(nCol) + " columns" )
 
-# Correlation matrix
-# def plotCorrelationMatrix(df, graphWidth):
-#     filename = df.dataframeName
-#     df = df.dropna('columns') # drop columns with NaN
-#     df = df[[col for col in df if df[col].nunique() > 1]] # keep columns where there are more than 1 unique values
-#     if df.shape[1] < 2:
-#         print(f'No correlation plots shown: The number of non-NaN or constant columns ({df.shape[1]}) is less than 2')
-#         return
-#     corr = df.corr()
-#     plt.figure(num=None, figsize=(graphWidth, graphWidth), dpi=80, facecolor='w', edgecolor='k')
-#     corrMat = plt.matshow(corr, fignum = 1)
-#     plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
-#     plt.yticks(range(len(corr.columns)), corr.columns)
-#     plt.gca().xaxis.tick_bottom()
-#     plt.colorbar(corrMat)
-#     plt.title(f'Correlation Matrix for {filename}', fontsize=15)
-#     plt.show()
-
-# plotCorrelationMatrix(df1, 10)
-
-# print(df1.loc[:, "University Rating"])
-# print(df1.loc[:, "Chance of Admit "])
+# np.random.seed(200)
+# k = 3
+# # centroids[i] = [x, y]
+# centroids = {
+#     i+1: [np.random.randint(0, 80), np.random.randint(0, 80)]
+#     for i in range(k)
+# }
+    
+# fig = plt.figure(figsize=(5, 5))
+# plt.scatter(df['x'], df['y'], color='k')
+# colmap = {1: 'r', 2: 'g', 3: 'b'}
+# for i in centroids.keys():
+#     plt.scatter(*centroids[i], color=colmap[i])
+# plt.xlim(0, 80)
+# plt.ylim(0, 80)
+# plt.show()
